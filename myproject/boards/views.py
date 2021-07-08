76438a6ee7e4e3ecd8556c36570a88b5ba05af75
harochau <harochau@gmail.com>
2021-07-08 17:52:44 +0300

    part 3

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -17,4 +17,7 @@ from .models import Board
 
 def home(request):
     boards = Board.objects.all()
-    return render(request,'home.html',{'boards':boards})
+    return render(request,'home.html', {'boards' : boards})
+
+def board_topics(request, pk):
+   pass
